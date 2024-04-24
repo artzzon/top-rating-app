d03fc4993ac9976578517c4c50edc19befed01eb
artzzon <nexus.artem@gmail.com>
2024-04-24 23:21:17 +0300

    prepairing to dev project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Noto_Sans } from "next/font/google";
+
+const notoSans = Noto_Sans({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["cyrillic", "latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={notoSans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
