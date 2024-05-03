46cad9e8c7e206bca02e92cd2136ebef8d505349
artzzon <nexus.artem@gmail.com>
2024-05-04 01:20:34 +0300

    add grid layout on page & layout footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { FooterProps } from "./Footer.props";
+import cn from "classnames";
+import styles from "./Footer.module.css";
 
-const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer {...props} className={cn(className, styles.footer)}>
+      <div>
+        OwlTop &copy; 2020 - {new Date().getFullYear()} Все права защищены
+      </div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,16 +4,16 @@ import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
 import Footer from "./Footer/Footer";
 
+import styles from "./Layout.module.css";
+
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
