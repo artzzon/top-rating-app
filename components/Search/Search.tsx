ceea0250803ffc6e64f8826a39bb85fe355adaca
artzzon <nexus.artem@gmail.com>
2024-06-09 23:09:24 +0300

    activate search button

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { KeyboardEvent } from "react";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
 import SearchIcon from "./Search.svg";
 import cn from "classnames";
 import styles from "./Search.module.css";
 import { SearchProps } from "./Search.props";
+import { useRouter } from "next/router";
 
 const Search = ({ className, ...props }: SearchProps) => {
-  const [search, setSearch] = React.useState("");
+  const [search, setSearch] = React.useState<string>("");
+  const router = useRouter();
+
+  const goToSearch = () => {
+    router.push({
+      pathname: "/search",
+      query: { q: search },
+    });
+  };
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      goToSearch();
+    }
+  };
 
   return (
     <div className={cn(className, styles.search)}>
       <Input
+        className={styles.input}
         placeholder="Поиск..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
-      <Button appearance="primary" className={styles.button} onClick={() => {}}>
+      <Button
+        appearance="primary"
+        className={styles.button}
+        onClick={goToSearch}
+      >
         <SearchIcon />
       </Button>
     </div>
